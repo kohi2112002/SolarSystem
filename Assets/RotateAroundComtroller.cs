d57427722f84883dd2d652ec9716d14f154cfce6
DESKTOP-BRSVPJH\kohi2 <ruakhoi@gmail.com>
2021-03-01 21:59:07 +0700

    final commit

---
+++ Assets/RotateAroundComtroller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundComtroller : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        transform.RotateAround(transform.position ,Vector3.up, speed * Time.deltaTime);
+    }
+}
